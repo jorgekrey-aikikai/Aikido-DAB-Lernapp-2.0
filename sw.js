d66fb23d13d703a7b7a74dd7e26eca26a629ccd0
jorgekrey-aikikai <jorge.krey@gmail.com>
2026-01-31 23:33:27 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'aikido-app-v2.1';
+const urlsToCache = [
+    'index.html',
+    'manifest.json',
+    'icons/icon-192x192.png',
+    'icons/icon-512x512.png'
+];
+
+// Install: App-Dateien cachen
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch: aus Cache oder Netzwerk
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) return response;
+            return fetch(event.request);
+        })
+    );
+});
+
+// Activate: alte Caches löschen
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+});
